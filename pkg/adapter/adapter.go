c5c863244c361b5a5524a886571bc925ab9463f6
Borja Aranda <me@fbac.dev>
2023-01-09 16:12:35 +0100

    Migrate to Run()

--- pkg/adapter/adapter.go
+++ pkg/adapter/adapter.go
@@ -1,20 +1,13 @@
 package adapter
 
-import (
-	//"fmt"
-	"github.com/fbac/adapter-demo/pkg/extlib"
-)
-
 type CompatibilityAdapter interface {
-	Reader() interface{}
-	Writer() interface{}
+	Run()
 }
 
-type Adapter struct{}
+type Adapter struct {
+	// Data, tests, etc
+}
 
-func (a Adapter) Run(rw any) {
-	//fmt.Printf("%T\n", a)
-	//fmt.Printf("%T\n", rw)
-	rw.(*extlib.ExtClient).ReadBalance("adapter")
-	rw.(*extlib.ExtClient).WriteBalance("adapter")
+func (a Adapter) Run(rw CompatibilityAdapter) {
+	rw.Run()
 }
--- pkg/extlib/extlib.go
+++ pkg/extlib/extlib.go
@@ -6,6 +6,14 @@ type ExtClient struct {
 	Id string
 }
 
+func (ec *ExtClient) ReadBalance(location string) {
+	fmt.Printf("ExtClient %s reading balance from %s\n", ec.Id, location)
+}
+
+func (ec *ExtClient) WriteBalance(location string) {
+	fmt.Printf("ExtClient %s writing balance from %s\n", ec.Id, location)
+}
+
 func NewClient(id string) *ExtClient {
 	return &ExtClient{
 		Id: id,
@@ -25,15 +33,12 @@ type ReaderWriter interface {
 	Writer
 }
 
-func (ec *ExtClient) ReadBalance(location string) {
-	fmt.Printf("ExtClient %s reading balance from %s\n", ec.Id, location)
-}
-
-func (ec *ExtClient) WriteBalance(location string) {
-	fmt.Printf("ExtClient %s writing balance from %s\n", ec.Id, location)
-}
-
 func ReaderWriterReadWrite(r ReaderWriter) {
 	r.ReadBalance("ReaderWriter interface")
 	r.WriteBalance("ReaderWriter interface")
+}
+
+func (ec *ExtClient) Run() {
+	ec.ReadBalance("run")
+	ec.WriteBalance("run")
 }
